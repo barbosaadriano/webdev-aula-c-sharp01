71251eec8d9c47377367e2ae5008ccc89a0a349f
adriano <b.adrianobarbosa@gmail.com>
2017-04-08 09:28:32 -0300

    ajustes

---
+++ WebSite/App_Code/Classes/Categoria.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Categoria
+/// </summary>
+public class Categoria
+{
+    #region Attr
+    private int cat_id;
+    private string cat_nome;
+
+    public int Cat_id {
+        get { return cat_id; }
+    }
+    public string Cat_nome {
+        get { return cat_nome;  }
+        set { cat_nome = value; }
+    }
+
+    #endregion
+    
+    public Categoria()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
